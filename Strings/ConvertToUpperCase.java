375b25b0721696c867e72c21d6f5c750faa8b932
BhabarnavaBuzarbaruah <bhabarnavabuzarbaruah@gmail.com>
2025-04-09 20:03:15 +0530

    Create ConvertToUpperCase.java

---
+++ Strings/ConvertToUpperCase.java
@@ -0,0 +1,20 @@
+public static String toUpperCase(String name) { // TC: O(n)
+    StringBuilder sb = new StringBuilder("");
+
+    // Capitalize the first character of the string
+    sb.append(Character.toUpperCase(name.charAt(0)));
+
+    // Traverse the rest of the string
+    for (int i = 1; i < name.length(); i++) {
+        // If current character is a space and the next character exists
+        if (name.charAt(i) == ' ' && i < name.length() - 1) {
+            sb.append(name.charAt(i)); // Append the space
+            i++; // Move to the next character
+            sb.append(Character.toUpperCase(name.charAt(i))); // Capitalize next word's first character
+        } else {
+            sb.append(name.charAt(i)); // Append character as it is
+        }
+    }
+
+    return sb.toString();
+}
