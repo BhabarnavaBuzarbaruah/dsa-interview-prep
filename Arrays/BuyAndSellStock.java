acdeefd7a849326c9fff202a4f52cf1b4f4037e1
BhabarnavaBuzarbaruah <bhabarnavabuzarbaruah@gmail.com>
2025-04-09 19:54:08 +0530

    Update BuyAndSellStock.java

--- Arrays/BuyAndSellStock.java
+++ Arrays/BuyAndSellStock.java
@@ -1,3 +1,6 @@
+// Problem: Best Time to Buy and Sell Stock
+// You are given an array where the ith element is the price of a given stock on day i.
+// Find the maximum profit you can achieve. You can only buy once and sell once.
 public static int maxProfit(int[] prices) {
         if (prices == null || prices.length == 0) return 0;
 
